60f542175f18b2f5b67b060f5f3de0c538d724d9
Dellvin <dellvin.black@gmail.com>
2020-04-25 01:29:38 +0300

    delete build-release

--- cmake-build-release/Tests/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dellvin/Desktop/Технопарк/Углубленное программирование С С++/HttpServer/cmake-build-release/Tests/googletest-src
-# Build directory: /Users/dellvin/Desktop/Технопарк/Углубленное программирование С С++/HttpServer/cmake-build-release/Tests/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-release/Tests/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dellvin/Desktop/Технопарк/Углубленное программирование С С++/HttpServer/cmake-build-release/Tests/googletest-src/googletest
-# Build directory: /Users/dellvin/Desktop/Технопарк/Углубленное программирование С С++/HttpServer/cmake-build-release/Tests/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
